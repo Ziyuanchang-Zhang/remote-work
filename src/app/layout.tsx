cbad20a1e71f06bc9fc69070ad40e350b264d210
ChloeZ <90809175+Ziyuanchang-Zhang@users.noreply.github.com>
2025-08-06 17:53:22 +0800

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+
+export const metadata: Metadata = {
+  title: "Deepwiki Open Source | Sheing Ng",
+  description: "Created by Sheing Ng",
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={"antialiased"}
+      >
+        <ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
+          <LanguageProvider>
+            {children}
+          </LanguageProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
